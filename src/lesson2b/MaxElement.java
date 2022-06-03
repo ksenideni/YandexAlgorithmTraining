dc5073a0db3d58b5ff1df09f28059980d14de72a
ksenideni <denishchenkovakseni@mail.ru>
2022-06-03 21:20:38 +0300

    task 1 in 2nd lesson

---
+++ src/lesson2b/MaxElement.java
@@ -0,0 +1,24 @@
+package lesson2b;
+
+import java.util.Scanner;
+
+public class MaxElement {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();
+        int max = i;
+        int count = 0;
+        while (i > 0) {
+            if (i > max) {
+                max = i;
+                count = 1;
+            } else {
+                if (i == max) {
+                    count++;
+                }
+            }
+            i = in.nextInt();
+        }
+        System.out.println(count);
+    }
+}
